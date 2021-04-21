c518fa7a31dd001f1777338a933fbe80fb648f9e
lightning720z <82703129+lightning720z@users.noreply.github.com>
2021-04-21 09:51:18 +0900

    Add files via upload

---
+++ For_Loop.py
@@ -0,0 +1,8 @@
+# belajar for loop
+
+pelanggan =  ["eko", "budi", "joko", "andi"]
+
+# mengakses semua nama pelanggan
+for nama in pelanggan:
+       print(nama)
+       print(f"Nama Pelanggan : {nama}")
